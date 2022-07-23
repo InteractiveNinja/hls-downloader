4b15729846d67680f33afefe6b0bf3287a90377f
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-07-23 19:30:52 +0200

    uses guard claws for testing arguments

--- src/cli.js
+++ src/cli.js
@@ -5,15 +5,23 @@ import { downloadFromCLIArgs, downloadFromBatchFile } from "./index.js";
 const args = yargs(hideBin(process.argv))
   .usage("$0 -i <URL with master.m3u8> -o <Output File path>")
   .usage("$0 -b <Path to Input file>").argv;
-
 const inputUrl = args.i;
 const outputPath = args.o;
 const batchFile = args.b;
 
-if (inputUrl && outputPath) {
-  downloadFromCLIArgs(inputUrl, outputPath);
-} else if (batchFile) {
-  downloadFromBatchFile(batchFile);
-} else {
+function run() {
+  if (inputUrl && outputPath) {
+    downloadFromCLIArgs(inputUrl, outputPath);
+    return;
+  }
+  if (batchFile) {
+    downloadFromBatchFile(batchFile);
+    return;
+  }
+
   console.log("No Arguments set");
 }
+
+(() => {
+  run();
+})();
