fb78d8b6861eec0dee4f2331f3a95afae4be283c
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-07-23 20:40:05 +0200

    adds function comment for default crawl function

--- src/crawler.js
+++ src/crawler.js
@@ -2,6 +2,11 @@ import voeCrawler from "./webcrawlers/voe.crawler.js";
 
 const registeredCrawler = [voeCrawler];
 
+/**
+ * Returns URL from a crawler
+ * @param url
+ * @returns {Promise<string>}
+ */
 export default async function crawl(url) {
   return registeredCrawler
     .map(async (crawler) => {
