93c6f3b6700e8c606f8fb3ac084d87d579057a7f
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-07-23 20:39:41 +0200

    simplifies crawler finder

--- src/crawler.js
+++ src/crawler.js
@@ -3,11 +3,10 @@ import voeCrawler from "./webcrawlers/voe.crawler.js";
 const registeredCrawler = [voeCrawler];
 
 export default async function crawl(url) {
-  return await registeredCrawler
+  return registeredCrawler
     .map(async (crawler) => {
-      const masterURL = await crawler(url);
-      return masterURL ?? undefined;
+      return await crawler(url);
     })
-    .filter((url) => !!url)
+    .filter((urls) => !!urls)
     .pop();
 }
