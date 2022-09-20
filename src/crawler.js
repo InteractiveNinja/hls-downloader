58f63fa6975d2f615cc3e07f9e76fa40354808c5
InteractiveNinja <gabrielnadolny2@gmail.com>
2022-09-20 10:35:40 +0200

    adds and uses @cantfindkernel/ddos-guard-bypass

--- src/crawler.js
+++ src/crawler.js
@@ -3,19 +3,19 @@ import voeCrawler from "./webcrawlers/voe.crawler.js";
 /**
  * Array of Crawlers
  * Crawler needs a default function that returns the master url or undefined
- * @type {((function(url: string): (Promise<string> | undefined)))[]}
+ * @type {((function(url: string): Promise<string>))[]}
  */
 const registeredCrawler = [voeCrawler];
 
 /**
  * Returns URL from a crawler
  * @param url
- * @returns {Promise<string | undefined>}
+ * @returns {Promise<string>}
  */
 export default async function crawl(url) {
   return registeredCrawler
     .map(async (crawler) => {
-      return crawler(url);
+      return await crawler(url);
     })
     .filter((urls) => !!urls)
     .pop();
--- src/webcrawlers/voe.crawler.js
+++ src/webcrawlers/voe.crawler.js
@@ -2,6 +2,7 @@ import jquery from "jquery";
 import { JSDOM } from "jsdom";
 import axios from "axios";
 import prompt from "prompt";
+import { bypass } from "@cantfindkernel/ddos-guard-bypass";
 
 const promptSchema = {
   properties: {
@@ -17,49 +18,50 @@ const promptSchema = {
 /**
  * Returns Promise
  * @param url
- * @returns {Promise<string> | undefined}
+ * @returns {Promise<string>}
  */
 export default async function voeCrawler(url) {
   const regexPattern = /https:\/\/[a-zA-Z\d.]*\/[a-zA-Z\d\/]*/;
-  const match = regexPattern.test(url);
-  if (!match) {
-    return undefined;
+  if (!regexPattern.test(url)) {
+    return Promise.reject();
   }
   prompt.start();
   console.log("Matched Crawler: voe/vupload");
-  return await prompt
-    .get(promptSchema)
-    .then(({ isVoe }) => {
-      if (isVoe === "n") {
-        return Promise.reject();
-      }
-      return crawUrl(url);
-    })
-    .catch(() => undefined);
+  return await prompt.get(promptSchema).then(({ isVoe }) => {
+    if (isVoe === "n") {
+      return Promise.reject();
+    }
+    return crawUrl(url);
+  });
 }
 
 async function crawUrl(url) {
-  return axios.get(url).then((res) => {
-    const { window } = new JSDOM(res.data);
-    const $ = jquery(window);
-    let scriptTags = [];
-    // Collect all script tags in website
-    $("script").each(function () {
-      const scriptTag = $(this).html();
-      scriptTags.push(scriptTag);
+  let bypassData = await bypass(url, true);
+  return axios({ url, headers: bypassData.headers })
+    .then((res) => {
+      const { window } = new JSDOM(res.data);
+      const $ = jquery(window);
+      let scriptTags = [];
+      // Collect all script tags in website
+      $("script").each(function () {
+        const scriptTag = $(this).html();
+        scriptTags.push(scriptTag);
+      });
+      return scriptTags
+        .join()
+        .split("\n")
+        .map((line) => line.trim()) // trim all lines
+        .join()
+        .split('"') // split to all "
+        .filter((line) => {
+          // find url with finding master file
+          const urlMatch =
+            /[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&/=,]*(\.m3u8))/;
+          return urlMatch.test(line);
+        })
+        .pop();
+    })
+    .catch((e) => {
+      throw new Error(`${e.response.status} - ${e.response.statusText}`);
     });
-    return scriptTags
-      .join()
-      .split("\n")
-      .map((line) => line.trim()) // trim all lines
-      .join()
-      .split('"') // split to all "
-      .filter((line) => {
-        // find url with finding master file
-        const urlMatch =
-          /[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&/=,]*(\.m3u8))/;
-        return urlMatch.test(line);
-      })
-      .pop();
-  });
 }
